ed12cc7786cbcbce04ccadb1880a634c4cccbf26
Bonaeineza31 <b.ineza@alustudent.com>
2025-02-27 12:27:57 +0200

    added users contact

--- models/userModal.js
+++ models/userModal.js
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 
+const { model, Schema } = mongoose;
 
-const{model,Schema}=mongoose;
-
-const userschema=schema(
-    {
-        userName:{
-            type:String,
-            required:true
-        },
-        userEmail:{
-            type:String,
-            required:true
-        },
-        userPassword:{
-            type:String,
-            required:true
-        },
-        userRole:{
-            type:String,
-            required:true,
-            default:user,
-            enum:"user, admin"
-        },
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true
+    },
+    userEmail: {
+      type: String,
+      required: true
+    },
+    userPassword: {
+      type: String,
+      required: true
+    },
+    userRole: {
+      type: String,
+      required: true,
+      default: "user",
+      enum: ["user", "admin"]
+    },
+    tokens: {
+      accessToken: {
+        type: String,
+      }
     }
-)
+  }
+);
+
+// Export the model
+const User = model("User", userSchema);
+export default User;
