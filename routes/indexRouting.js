2e2d3a926521e5dfe4762b2ca6a7ea57b7124657
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-01 00:01:27 +0200

    worked on index routing

--- routes/indexRouting.js
+++ routes/indexRouting.js
@@ -2,11 +2,12 @@ import express from 'express';
 import contactRouter from './contactPath.js';
 import productRouter from './productPath.js';
 import userRouter from './userPath.js';
-
+import blogRouter from "./blogpath.js";
 
 const mainRouter= express.Router();
 mainRouter.use('/contact', contactRouter);
 mainRouter.use('/products', productRouter);
 mainRouter.use('/user', userRouter);
+mainRouter.use("/blog",blogRouter);
 
 export default mainRouter;
